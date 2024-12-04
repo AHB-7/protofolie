d900f3ba05cca65c71b6d6094837a63f65a140af
AHB-7 <118571657+AHB-7@users.noreply.github.com>
2024-12-05 00:15:37 +0100

    gif it

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,20 @@
+import Image from "next/image";
+
 export default function Home() {
     return (
         <div className="w-full mx-auto p-1">
-            <h1 className=" text-9xl font-extrabold">Portofolie</h1>
+            {/* <h1 className="text-7xl md:text-9xl font-extrabold font-poppins text-center">
+                Portofolie
+            </h1> */}
+            <div className="absolute top-0 left-0 w-full h-screen flex items-center justify-center">
+                <Image
+                    className="mx-auto"
+                    src="/images/loading.gif"
+                    alt="alt"
+                    width={80}
+                    height={30}
+                />
+            </div>
         </div>
     );
 }
