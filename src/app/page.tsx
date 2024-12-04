7fffaa198b21c78998e94c11eca767e851d665cf
AHB-7 <118571657+AHB-7@users.noreply.github.com>
2024-12-04 23:16:53 +0100

    just installed the app

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
     return (
         <div className="w-full mx-auto p-1">
-            <h1 className=" text-9xl font-extrabold">Protofoilie</h1>
+            <h1 className=" text-9xl font-extrabold">Portofolie</h1>
         </div>
     );
 }
