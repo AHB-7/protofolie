eade098b4260767d39bccbf74a26704ff1dd0022
Alan Br <AHB-7@users.noreply.github.com>
2024-12-09 17:02:11 +0100

    skills

--- src/app/components/home/hero-title/hero-content.tsx
+++ src/app/components/home/hero-title/hero-content.tsx
@@ -22,14 +22,15 @@ export function Hero() {
         ["#000", "#fff"]
     );
     const scaleDown = useTransform(scrollYProgress, [0, 0.7], [1, 0.09]);
-    // const width = useTransform(scrollYProgress, [0, 0.8], ["100%", "5rem"]);
+    const display = useTransform(scrollYProgress, [0, 0.7], ["flex", "none"]);
+    const showOpacity = useTransform(scrollYProgress, [0, 0.7], [0, 1]);
     return (
         <motion.section
             ref={ref}
             className="relative overflow-hidden h-screen flex justify-center"
         >
             <motion.div
-                className="fixed border flex flex-col gap-4 py-12 mx-auto overflow-hidden w-[calc(100%-4rem)] h-[calc(100vh-8rem)]"
+                className="fixed border flex flex-col gap-4 py-12 mx-auto overflow-hidden w-[calc(100%-4rem)] h-[calc(100vh-8rem)] items-center justify-center "
                 style={{
                     borderRadius: borderRadius,
                     backgroundColor: backgroundScroll,
@@ -38,26 +39,43 @@ export function Hero() {
                     backgroundBlendMode: "screen",
                     backgroundSize: "cover",
                     backgroundPosition: "center",
-                    scale: scaleDown,
+                    zoom: scaleDown,
                 }}
             >
                 <motion.h1
                     className="text-4xl font-bold text-center z-20 pt-8 md:pt-20"
-                    style={{ opacity }}
+                    style={{ opacity, display }}
                 >
                     Hello, I&apos;m
                 </motion.h1>
-                <GlitchedText
-                    text="Alan Brim"
-                    class="text-6xl md:text-8xl text-center z-20 py-4"
-                />
+                <motion.div style={{ opacity, display }}>
+                    <GlitchedText
+                        text="Alan Brim"
+                        class="text-6xl md:text-8xl text-center z-20 py-4"
+                    />
+                </motion.div>
                 <motion.p
                     className="text-center text-2xl px-2 z-20 tracking-widest uppercase leading-8"
-                    style={{ opacity }}
+                    style={{ opacity, display }}
                 >
                     I&apos;m a Front-end Developer Loves new technologies and
                     web development.
                 </motion.p>
+                <motion.div
+                    className=" text-black text-center z-20 font-extrabold tracking-widest uppercase"
+                    style={{
+                        zoom: "2",
+                    }}
+                >
+                    <motion.h2
+                        style={{
+                            zoom: "20",
+                            opacity: showOpacity,
+                        }}
+                    >
+                        Skills
+                    </motion.h2>
+                </motion.div>
             </motion.div>
         </motion.section>
     );
